791f9550f269ece0b783cc4004d1e877877a62af
AtonKi <akenaton693@gmail.com>
2020-03-23 01:59:52 -0500

    Peli

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     text = { 'content': 'Welcome to Aton Code' }  
-    Titleparagraf ={'content': 'This website is for coders'}
-    paragrafOne= {'content': ' '}
+    Titleparagraf ={'content': 'The WarGames Movie'}
+    paragrafOne= {'content': 'Movie in Sapnish'}
    
     return render_template("index.html", paragrafOne= paragrafOne, title = '| The Aton Code Blog',text = text,  Titleparagraf=Titleparagraf)
 
