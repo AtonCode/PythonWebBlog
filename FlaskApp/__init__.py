ab6fdac5f264825058c9e0724dc9259382730d58
AtonKi <akenaton693@gmail.com>
2020-03-23 15:51:28 -0500

    1

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -5,11 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    titlePage = '| The Aton Code Blog'
     text = { 'content': 'Welcome to Aton Code' }  
     Titleparagraf ={'content': 'The WarGames Movie'}
     paragrafOne= {'content': 'Movie in Sapnish'}
    
-    return render_template("index.html", paragrafOne= paragrafOne, title = '| The Aton Code Blog',text = text,  Titleparagraf=Titleparagraf)
+    return render_template("index.html", paragrafOne= paragrafOne, titlePage=titlePage,text = text,  Titleparagraf=Titleparagraf)
 
 app.debug = True 
 if __name__ == "__main__":
