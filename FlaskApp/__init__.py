b7f6df25d6e2706d913cd2e0ce18659691b43fc8
AtonKi <akenaton693@gmail.com>
2020-03-23 01:27:44 -0500

    Template

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello, I am using Flask for that site"
+
+
+@app.route('/')
+def index():
+    text = { 'content': 'Welcome to Aton Code' }  
+    Titleparagraf ={'content': 'This website is for coders'}
+    paragrafOne= {'content': ' '}
+   
+    return render_template("index.html", paragrafOne= paragrafOne, title = '| The Aton Code Blog',text = text,  Titleparagraf=Titleparagraf)
+
     
 if __name__ == "__main__":
     app.run()
