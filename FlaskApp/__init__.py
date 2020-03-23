af8f3eaf7c53153278a8d65b39a70b1ff2bcded3
AtonKi <akenaton693@gmail.com>
2020-03-23 18:56:59 -0500

    UploadFunciona!

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -49,10 +49,10 @@ def allowed_file(filename):
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
-
+#---------------HTML-------------------------------#
     titlePage= '| Upload New File'
 
-
+#---------------------------------------------------#
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -67,8 +67,8 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+            return redirect(url_for('uploaded_file',filename=filename))
+
     return render_template('upload.html', titlePage= titlePage)
 
 
@@ -76,8 +76,7 @@ filename = "../../../../home/username/.bashrc"
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
 
 # Fin UploadFile
 
