c0d450aa68482b8a4187b94249c6422a957f851b
AtonKi <akenaton693@gmail.com>
2020-03-23 23:38:07 -0500

    remove MarkupSafe

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,5 +1,5 @@
 import os
-from markupsafe import Markup, escape
+import Markup, escape
 from werkzeug.utils import secure_filename
 from flask import Flask ,render_template, redirect, url_for, flash, request, send_from_directory
 
