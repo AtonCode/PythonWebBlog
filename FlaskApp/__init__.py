4766889a5c65523c0791fa20f2f7d9e6a5cab82a
AtonKi <akenaton693@gmail.com>
2020-03-27 13:34:52 -0500

    Añdiendo Rutas

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -19,6 +19,71 @@ def index():
         
         )
 
+# Rutas About
+@app.route('/about')
+def about():
+
+    titlePage = '| About'
+    text = { 'content': 'About' }  
+    Titleparagraf ={'content': 'Me'}
+    paragrafOne= {'content': 'I am'}
+   
+    return render_template(
+
+        "about.html",
+        titlePage= titlePage, 
+        paragrafOne= paragrafOne,
+        text = text,
+        Titleparagraf=Titleparagraf,
+       
+        )
+    
+# Fin Rutas About
+
+
+# Rutas Hobbies
+@app.route('/hobbies')
+def hobbies():
+
+    titlePage = '| Hobbies'
+    text = { 'content': 'Hobbies' }  
+    Titleparagraf ={'content': 'My Hobbies'}
+    paragrafOne= {'content': 'I am'}
+   
+    return render_template(
+
+        "hobbies.html",
+        titlePage= titlePage, 
+        paragrafOne= paragrafOne,
+        text = text,
+        Titleparagraf=Titleparagraf,
+       
+        )
+    
+# Fin Rutas Hobbies
+
+
+# Rutas Apps
+@app.route('/apps')
+def Apps():
+
+    titlePage = '| Apps'
+    text = { 'content': 'Apps' }  
+    Titleparagraf ={'content': 'Recomendate Apps'}
+    paragrafOne= {'content': 'Apps'}
+   
+    return render_template(
+
+        "apps.html",
+        titlePage= titlePage, 
+        paragrafOne= paragrafOne,
+        text = text,
+        Titleparagraf=Titleparagraf,
+       
+        )
+    
+# Fin Rutas Apps
+
  
     
 if __name__ == "__main__":
