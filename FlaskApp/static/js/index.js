b40f26a9214e42142e4233051db85c771c5268fd
AtonKi <akenaton693@gmail.com>
2020-03-27 20:32:02 -0500

    Js Termiando

--- FlaskApp/static/js/index.js
+++ FlaskApp/static/js/index.js
@@ -1,13 +1,21 @@
-jQuery('document').ready(function($)
-{
+
+jQuery('document').ready(function ($) {
 
     var menuBtn = $('.menu-icon'),
-    menu = $('.navigation ul');
+        menu = $('.navigation ul');
 
-    menuBtn.click(function(){
+    menuBtn.click(function () {
 
-        menu.addClass('show');
+        if(menu.hasClass('show')){
 
- });
+            menu.removeClass('show');
+        }else{
+
+            menu.addClass('show');
+        }
+
+        
+
+    });
 
 });
